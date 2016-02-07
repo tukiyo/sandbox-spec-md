1bb44e0927278a12749e9da6b6fabf9a2f81d4cf
tukiyo3 <tukiyo3@gmail.com>
2016-02-07 17:42:28 +0900

    fix

--- conv.sh
+++ conv.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 installcheck(){
-  test -e `which npm`     || sudo apt-get install -y npm
-  test -e `which spec-md` || sudo npm install -g spec-md
-  test -e `which node`    || ln -s /usr/bin/nodejs ~/bin/node
-  test -d dist -a -d specs|| mkdir dist specs
-  test -e dist/highlight.css || cp -a /usr/local/lib/node_modules/spec-md/css/highlight.css dist
-  test -e dist/spec.css || cp -a /usr/local/lib/node_modules/spec-md/css/spec.css dist
+  test -z `which npm`      && sudo apt-get install -y npm
+  test -z `which spec-md`  && sudo npm install -g spec-md
+  test -z `which node`     && ln -s /usr/bin/nodejs ~/bin/node
+  test -z dist -a -d specs && mkdir dist specs
+  test -z dist/highlight.css && cp -a /usr/local/lib/node_modules/spec-md/css/highlight.css dist
+  test -z dist/spec.css && cp -a /usr/local/lib/node_modules/spec-md/css/spec.css dist
   #npm install --save-dev spec-md
 }
 installcheck
